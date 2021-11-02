b8915c851889e5c9689856b3806230eaadfe708d
DESKTOP-6KSAIBM\AD <buiductinwork@gmail.com>
2021-11-02 14:44:30 +0700

    change to use json.stringify

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,5 +1,5 @@
-// const Base = 'https://chatbox-be.vercel.app'
-const Base = 'http://localhost:3005'
+const Base = 'https://chatbox-be-7yrf0ywqc-dustinonthemic.vercel.app'
+// const Base = 'http://localhost:3005'
 
 
 export const ApiUrl = Object.freeze({
