ec2a3475f6413bba8355352141f2414427101d1f
DESKTOP-6KSAIBM\AD <buiductinwork@gmail.com>
2021-11-02 10:52:49 +0700

    done

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -2,7 +2,7 @@ import { Avatar, Stack } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/system";
 import clsx from "clsx";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const useStyle = makeStyles({
   message: {
@@ -26,6 +26,11 @@ const useStyle = makeStyles({
   messageLeft: {
     background: '#54a0ff',
     color: "#FFFFFF"
+  },
+  avatar: {
+    height: "40px",
+    width: "40px",
+    borderRadius: "50%"
   }
 })
 
@@ -33,6 +38,18 @@ const Message = ({message}) => {
   const classes = useStyle()
   const userInfo = JSON.parse(localStorage.getItem('userInfo'))
   const userId = userInfo ? userInfo.id : null
+  const [img, setImg] = useState(null)
+
+  useEffect(() => {
+    const img = new Image()
+    img.crossOrigin = "anonymous"
+    img.src = message.ava
+    setImg(img)
+  }, [message.ava])
+
+  const handleShowImg = () => {
+    window.open(message.ava, "_blank")
+  }
 
   return (
       <Stack 
@@ -44,7 +61,7 @@ const Message = ({message}) => {
         direction={userId && userId === message.id ? 'row-reverse' : 'row'}
         spacing={2}
       >
-        <Avatar src={message.ava}/>
+        <img className={classes.avatar} crossOrigin="anonymous" src={message.ava} onClick={handleShowImg}/>
         <Box className={clsx(classes.content, userId && message.id === userId && classes.messageLeft)}>
           {message.content}
         </Box>
